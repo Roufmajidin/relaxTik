198caf8971ddebd0b9024ce44221e6b9f9cf0cdd
tribber93 <tribberyoni5@gmail.com>
2023-07-26 23:01:06 +0700

    tambah detail transaksi

---
+++ lib/view/detail_transaksi.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+
+class DetailTransaksi extends StatefulWidget {
+  const DetailTransaksi({super.key});
+
+  @override
+  State<DetailTransaksi> createState() => _DetailTransaksiState();
+}
+
+class _DetailTransaksiState extends State<DetailTransaksi> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromRGBO(199, 223, 240, 1),
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        title: const Center(child: Text('Ringkasan Order')),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Image.asset('assets/images/logo.png'),
+          )
+        ],
+      ),
+      extendBodyBehindAppBar: true,
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: SafeArea(
+            child: SingleChildScrollView(
+          child: Container(
+            margin: const EdgeInsets.only(bottom: 50),
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50,
+                  // color: Colors.amber,
+                  child: Image.asset(
+                    'assets/icons/iconoir_profile-circle.png',
+                    height: 40,
+                  ),
+                ),
+                const SizedBox(height: 50),
+                const Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'Riwayat Transaksi',
+                    style: TextStyle(fontWeight: FontWeight.w600),
+                  ),
+                ),
+                const SizedBox(height: 30),
+                Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(20)),
+                  // constraints: BoxConstraints(minHeight: 200),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: 3,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            margin: const EdgeInsets.only(bottom: 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                Text(
+                                  "Nama Paket",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Text(
+                                  "2 * 15000",
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w500),
+                                ),
+                              ],
+                            ),
+                          );
+                        },
+                      ),
+                      SizedBox(height: 10),
+                      const Divider(thickness: 2, color: Colors.black),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 8),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Total",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w600),
+                            ),
+                            Text(
+                              "30000",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        backgroundColor: const Color.fromRGBO(114, 136, 214, 1),
+        onPressed: () {
+          // Add your onPressed code here!
+          Navigator.pushNamed(context, '/dashboard');
+        },
+        label: const Padding(
+          padding: EdgeInsets.all(25),
+          child: Text(
+            'Bayar Sekarang',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        // backgroundColor: Colors.pink,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+}
